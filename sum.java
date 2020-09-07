e020d737acf65fc0e4d7a94416f936b6046801fe
Shreya01Reddy <70909295+Shreya01Reddy@users.noreply.github.com>
2020-09-07 22:43:56 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class sum
+{
+  public static void main(string[] args)
+  {
+   int num1=5, num2=15, sum;
+   sum=num1+num2;
+   System.out.println("sum of these two numbers= "+sum);
+   }
+}
